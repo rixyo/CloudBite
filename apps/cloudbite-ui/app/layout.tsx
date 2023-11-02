065f13bf4ffeaaef776e418e171b9f1cdd1a1242
rixyo <saleh35-3063@diu.edu.bd>
2023-11-03 01:16:21 +0600

    first-commit-ui

---
+++ apps/cloudbite-ui/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'CloudBite: A Cloud-Native Food Delivery Experience​',
+  description: 'CloudBite: A Cloud-Native Food Delivery Experience​',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ apps/cloudbite-ui/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="">
+     <h1>
+      Checking is everything is working
+     </h1>
+    </main>
+  )
+}
